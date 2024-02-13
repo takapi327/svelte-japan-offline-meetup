7daaa596941c8d1c7bda9678346c2d5bb2eda2ff
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-02-13 23:54:08 +0900

    Create svelte34-event-delivery

---
+++ examples/svelte34-event-delivery/src/lib/component/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  //import { createEventDispatcher } from 'svelte'
+
+  //const dispatch = createEventDispatcher()
+
+  //function sayHello() {
+  //  dispatch('click', {
+  //    text: 'こんにちは!'
+  //  })
+  //}
+  export let sayHello: (text: string) => void
+</script>
+
+<button on:click={() => sayHello('こんにちは!')}>
+  クリック
+</button>
---
+++ examples/svelte34-event-delivery/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Button from '../lib/component/Button.svelte'
+
+  //function handleMessage(event) {
+  //  alert(event.detail.text)
+  //}
+  function sayHello(text: string) {
+    alert(text)
+  }
+</script>
+
+<Button {sayHello} />
